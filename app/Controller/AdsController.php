e06f6babb054c7d1c0d8711a74af83478a48d022
Bryan Sumague <bryan.sumague09@gmail.com>
2014-06-02 14:07:09 +0800

    Installed Foundation

---
+++ app/Controller/AdsController.php
@@ -0,0 +1,21 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Users Controller
+ *
+ * @property User $User
+ * @property PaginatorComponent $Paginator
+ */
+class UsersController extends AppController {
+
+/**
+ * Main View
+ *
+ * @param none
+ * @return void
+ */
+	public function index() {
+		
+	}
+
+}
