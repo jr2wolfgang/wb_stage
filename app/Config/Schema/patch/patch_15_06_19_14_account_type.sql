b954a553e45d0af4e6a919b41e246e1dafb07cc6
bien relampagos <bienrelampagos@gamil.com>
2014-06-19 20:29:27 -0700

    patch account type and group table

---
+++ app/Config/Schema/patch/patch_15_06_19_14_account_type.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.6.16 : Database - nakatipid
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`nakatipid` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `nakatipid`;
+
+/*Table structure for table `account_types` */
+
+DROP TABLE IF EXISTS `account_types`;
+
+CREATE TABLE `account_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `account_types` */
+
+insert  into `account_types`(`id`,`name`,`created`,`modified`,`modified_by`) values (1,'individual','0000-00-00 00:00:00','0000-00-00 00:00:00',0),(2,'services','0000-00-00 00:00:00','0000-00-00 00:00:00',0),(3,'business','0000-00-00 00:00:00','0000-00-00 00:00:00',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
