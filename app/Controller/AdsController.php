3fec1cbaf292febdcaa05c331f863c51484c0a09
jr <jr@ggnet.com>
2014-06-20 19:54:06 +0800

    test 2

--- app/Controller/AdsController.php
+++ app/Controller/AdsController.php
@@ -24,6 +24,7 @@ class AdsController extends AppController {
 
 	public function index() {
 		echo 'test ito';
+		echo 'isa pa';
 		$this->Ad->bind(array('Image','PrimaryImage','Address'));
 
 		$this->paginate = array(
